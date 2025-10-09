77604287ea05075d1926cbf04d98b761d35b8c8f
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-10-09 14:50:36 -0400

    Update Site

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ const config = {
   },
   "settings": {
     "confettiColors": [
-      "#C62828",
+      "#800020",
       "#2E7D32",
       "#FFFFFF"
     ],
@@ -184,7 +184,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1760035435627_0",
+      "id": "q_auto_1760035834948_0",
       "questionText": "What do you like the most about your current home?",
       "options": [
         {
@@ -206,7 +206,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1760035435627_1",
+      "id": "q_auto_1760035834948_1",
       "questionText": "Is a move on your radar in the near future?",
       "options": [
         {
@@ -228,7 +228,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1760035435627_2",
+      "id": "q_auto_1760035834948_2",
       "questionText": "What’s your current housing situation?",
       "options": [
         {

23507268bbb52d06feda54ae68d252df7deaf0d5
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-10-09 14:56:54 -0400

    Update Site

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const config = {
     ],
     "showCountdownInHero": true,
     "theme": "dark",
-    "primaryColorOverride": "#8E4585",
+    "primaryColorOverride": "#800020",
     "secondaryColorOverride": "",
     "ghlWebhookUrl": "https://services.leadconnectorhq.com/hooks/bJbBMjcW619cJrLUJ3kY/webhook-trigger/3yNsurfrt9oBUtRBuygb",
     "facebookPixelId": "1057475448873422",
@@ -184,7 +184,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1760036169100_0",
+      "id": "q_auto_1760036213951_0",
       "questionText": "What do you like the most about your current home?",
       "options": [
         {
@@ -206,7 +206,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1760036169100_1",
+      "id": "q_auto_1760036213951_1",
       "questionText": "Is a move on your radar in the near future?",
       "options": [
         {
@@ -228,7 +228,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1760036169100_2",
+      "id": "q_auto_1760036213951_2",
       "questionText": "What’s your current housing situation?",
       "options": [
         {

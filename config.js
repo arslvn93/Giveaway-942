5bc17d8a7a83e4ab92c16188c98af6c216bf90e2
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-10-14 12:40:26 -0400

    Update Site

--- config.js
+++ config.js
@@ -184,7 +184,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1760460024240_0",
+      "id": "q_auto_1760460025841_0",
       "questionText": "What do you like the most about your current home?",
       "options": [
         {
@@ -206,7 +206,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1760460024240_1",
+      "id": "q_auto_1760460025841_1",
       "questionText": "Is a move on your radar in the near future?",
       "options": [
         {
@@ -228,7 +228,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1760460024240_2",
+      "id": "q_auto_1760460025841_2",
       "questionText": "What’s your current housing situation?",
       "options": [
         {
